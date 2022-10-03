2a02925ae62de349263c15e594d6115579a6175b
그룹웨어 장성욱 <seonguk94@naver.com>
2022-10-03 14:08:02 +0900

    CreatorController view

--- src/main/java/com/controller/CreatorController.java
+++ src/main/java/com/controller/CreatorController.java
@@ -1,4 +1,16 @@
 package com.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
 public class CreatorController {
+    @GetMapping(value="/playpage.do")
+    public ModelAndView selectPlayPage(HttpServletRequest request, ModelAndView mav){
+        mav.setViewName("/playpage.do");
+        return mav;
+    }
 }
