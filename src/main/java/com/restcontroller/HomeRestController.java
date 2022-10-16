b1c3561b97673aa7f0d5d725aaf770ca0090f53c
그룹웨어 장성욱 <seonguk94@naver.com>
2022-10-16 23:25:11 +0900

    파일업로드(thumnail, videoFile)

--- src/main/java/com/restcontroller/HomeRestController.java
+++ src/main/java/com/restcontroller/HomeRestController.java
@@ -1,6 +1,9 @@
 package com.restcontroller;
 
+import com.aws.file.FileUploadUtility;
+import com.google.gson.Gson;
 import com.model.Video;
+import com.model.common.MFile;
 import com.response.DefaultRes;
 import com.response.Message;
 import com.service.HomeService;
@@ -10,6 +13,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
@@ -20,12 +24,31 @@ import java.util.ArrayList;
 public class HomeRestController {
 
     private final HomeService homeService;
+    private final FileUploadUtility fileUploadUtility;
 
     @PostMapping("/test/main")
     public ResponseEntity videoInsert(HttpServletRequest request, Video video) {
+        Gson gson = new Gson();
         Message message = new Message();
         //ArrayList<String> strings = new ArrayList<>();
         //strings.add("blahblah");
+        MFile mFile = null;
+        if (video.getVideo_file() == null || video.getVideo_file().getSize() == 0) {
+            message.put("status", false);
+        } else {
+            mFile = fileUploadUtility.uploadFile(video.getVideo_file(), null);
+            //GSON 변환
+            String uploadVideo = gson.toJson(mFile);
+            video.setVideoFile(uploadVideo);
+        }
+
+        if (video.getThumbnail_file() == null || video.getThumbnail_file().getSize()==0){
+            message.put("status", false);
+        } else {
+            mFile = fileUploadUtility.uploadFile(video.getThumbnail_file(), null);
+            String uploadThumbnail = gson.toJson(mFile);
+            video.setThumbnail(uploadThumbnail);
+        }
         message.put("status", true);
         int videoNum = homeService.insertVideo(video);
         log.info(String.valueOf(video));
