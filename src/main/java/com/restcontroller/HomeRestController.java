1b7c88c1c5e67a68e5032a6fb69834b1676b4ca3
Tissue94 <tissue2979@naver.com>
2022-11-13 23:58:09 +0900

    Revert "infinity scroll 70% complete"
    This reverts commit 46b5820d

--- src/main/java/com/restcontroller/HomeRestController.java
+++ src/main/java/com/restcontroller/HomeRestController.java
@@ -1,6 +1,5 @@
 package com.restcontroller;
 
-import com.api.sns.kakao.KakaoInfo;
 import com.aws.file.FileUploadUtility;
 import com.google.gson.Gson;
 import com.model.Video;
@@ -10,17 +9,16 @@ import com.response.Message;
 import com.service.HomeService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import net.bramp.ffmpeg.FFprobe;
-import net.bramp.ffmpeg.probe.FFmpegFormat;
-import net.bramp.ffmpeg.probe.FFmpegProbeResult;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
-import java.io.IOException;
 import java.util.ArrayList;
 
 @Slf4j
@@ -32,23 +30,12 @@ public class HomeRestController {
     private final FileUploadUtility fileUploadUtility;
 
     @PostMapping("/test/main")
-
     public ResponseEntity videoInsert(HttpServletRequest request, Video video) {
-
         Gson gson = new Gson();
         Message message = new Message();
         //ArrayList<String> strings = new ArrayList<>();
         //strings.add("blahblah");
         MFile mFile = null;
-
-        if (video.getThumbnail_file() == null || video.getThumbnail_file().getSize()==0){
-            message.put("status", false);
-        } else {
-            mFile = fileUploadUtility.uploadFile(video.getThumbnail_file(), null);
-            String uploadThumbnail = gson.toJson(mFile);
-            video.setThumbnail(uploadThumbnail);
-        }
-
         if (video.getVideo_file() == null || video.getVideo_file().getSize() == 0) {
             message.put("status", false);
         } else {
@@ -58,46 +45,33 @@ public class HomeRestController {
             video.setVideoFile(uploadVideo);
         }
 
-        message.put("status", true);
-
-        String returnData = "0";
-
-        try {
-            FFprobe ffprobe = new FFprobe("C:/Users/seong/Downloads/ffmpeg-5.1.2-essentials_build/ffmpeg-5.1.2-essentials_build/bin/ffprobe.exe"); // window에 설치된  ffprobe.exe 경로
-            log.info(mFile.getUrl());
-            FFmpegProbeResult probeResult = ffprobe.probe(mFile.getUrl()); // 동영상 경로
-            FFmpegFormat format = probeResult.getFormat();
-            double second = format.duration; // 초단위
-            int videoSecond = (int) Double.parseDouble(String.valueOf(second));
-            Math.floor(videoSecond);
-
-            returnData = videoSecond + "";
-            System.out.println("second==" + videoSecond);
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            System.out.println("@@ media_player_time end @@");
+        if (video.getThumbnail_file() == null || video.getThumbnail_file().getSize()==0){
+            message.put("status", false);
+        } else {
+            mFile = fileUploadUtility.uploadFile(video.getThumbnail_file(), null);
+            String uploadThumbnail = gson.toJson(mFile);
+            video.setThumbnail(uploadThumbnail);
         }
-
-        log.info(returnData);
-        video.setTime(returnData);
+        message.put("status", true);
         int videoNum = homeService.insertVideo(video);
-
         log.info(String.valueOf(video));
         //message.put("strings", strings);
         return new ResponseEntity(DefaultRes.res(HttpStatus.OK, message), HttpStatus.OK);
     }
 
-//    public ResponseEntity JsonToObject(HttpServletRequest request) {
-//
-//
-//
-//
-//
-//        return new ResponseEntity(DefaultRes.res(HttpStatus.OK, message), HttpStatus.OK);
-//
-//    }
+    @RequestMapping(value = "/testPreview", method = RequestMethod.POST)
+    public ResponseEntity<String> uploadPreviewVideo(ModelAndView mav, HttpServletRequest request, MultipartFile video_file)throws Exception{
+        Message message = new Message();
+
+        MFile file = fileUploadUtility.uploadFile(video_file, null);
+        message.put("file", file);
+        message.put("url", file.getUrl());
+
+        mav.addObject("file", video_file);
+
+        return new ResponseEntity(DefaultRes.res(HttpStatus.OK, message), HttpStatus.OK);
+
+    }
 
 
 }
